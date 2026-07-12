51e4d49921ad0d22cd5b20224b73dd6d58882d49
enkhbold470 <enkhbold470@gmail.com>
2026-07-12 03:48:38 -0700

    Initial Commit

---
+++ neurosync/neurosyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  neurosyncApp.swift
+//  neurosync
+//
+//  Created by Inky Ganbold on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct neurosyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ neurosyncTests/neurosyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  neurosyncTests.swift
+//  neurosyncTests
+//
+//  Created by Inky Ganbold on 7/12/26.
+//
+
+import Testing
+@testable import neurosync
+
+struct neurosyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
